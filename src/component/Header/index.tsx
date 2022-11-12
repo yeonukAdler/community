54203626daea0deee0584ce53ce4af46d59c17a0
development-adler <88467632+development-adler@users.noreply.github.com>
2022-11-12 11:14:58 +0900

    비회원에게 게시판 노출 안되도록 변경

--- src/component/Header/index.tsx
+++ src/component/Header/index.tsx
@@ -2,19 +2,25 @@ import { HeaderContainer, HeaderButton, UserNameButton } from './styles';
 import { useNavigate } from 'react-router-dom';
 import { tokenAtom, userAtom } from 'atoms';
 import { useAtom, useAtomValue } from 'jotai';
+import { Path } from 'constant';
 
 function Header(): JSX.Element {
   const navigate = useNavigate();
   const user = useAtomValue(userAtom);
   const [userToken, setUserToken] = useAtom(tokenAtom);
 
+  const headerlogOutButton = () => {
+    setUserToken(undefined);
+    window.location.reload();
+  };
+
   return (
     <HeaderContainer>
-      <HeaderButton onClick={() => navigate('/')}>게시판</HeaderButton>
+      <HeaderButton onClick={() => navigate(`${Path.home}`)}>게시판</HeaderButton>
       {userToken ? (
         <>
           <UserNameButton>{user?.results.username}</UserNameButton>
-          <HeaderButton onClick={() => setUserToken(undefined)}>로그아웃</HeaderButton>
+          <HeaderButton onClick={headerlogOutButton}>로그아웃</HeaderButton>
         </>
       ) : (
         <>
